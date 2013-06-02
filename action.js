cbb09a3eec75158bc1562490fa1295cf7017c1ba
Sean Helvey <smh436@nyu.edu>
2013-06-02 15:27:08 -0600

    5 DOM dots sticking now, need to be adjusted.

--- action.js
+++ action.js
@@ -5,36 +5,57 @@ $(function() {
   	document.location.reload(true)
 	});
 
-  /*
-  $(".ui-widget-content").each(function( index ) {
-  	console.log( index + "left: " + $(this).position().left );
-  	console.log( index + "top: " + $(this).position().top );
-  	console.log( index + "right: " + $(this).position().right );
-   	console.log( index + "bottom: " + $(this).position().bottom ); 	  	  	
-	});*/
+	detectMouse("blue_dot");
+	detectMouse("red_dot");
+	detectMouse("green_dot");
+	detectMouse("1_black_dot");
+	detectMouse("2_black_dot");
 
-	$("#blue_dot").mousemove(function(event) {
-  var msg = "Handler for .mousemove() called at ";
-  msg += event.pageX + ", " + event.pageY;
-	console.log(msg);
-	controlDotLocation(event)
-	});
+  function detectMouse(dot_name){
+		$("#" + dot_name).mouseup(function(event) {
+			  var msg = "Handler for .mouseup() called at ";
+			  msg += event.pageX + ", " + event.pageY;
+				console.log(msg);
+				locationControl(dot_name,event);
+			});
+  }
 
-	function controlDotLocation(event){
-
-    if (event.pageX > 540 && event.pageX < 580
-    	&& event.pageY > 160 && event.pageY < 200) {
-    	console.log("got it!!!");
-    	snapIntoPlace();
-		//blue
-		//lr 530-590
-		//tp 150-210
-		}
+	function locationControl(dot_name, currentEvent){
+    if (dot_name === "blue_dot"){
+    	if (inRange(currentEvent, 540,580,160,200)){
+    		console.log("a");
+    		$("#blue_dot").off();
+    	}
+    } else if(dot_name === "red_dot"){
+    	if (inRange(currentEvent, 660,700,180,220)){
+    		console.log("b");    		
+    		$("#red_dot").off();
+    	}
+    } else if(dot_name === "green_dot"){
+    	if (inRange(currentEvent, 450,490,355,395)){
+    		console.log("c");    		
+	    	$("#green_dot").off();
+	    }
+    } else if(dot_name === "1_black_dot"){
+    	if (inRange(currentEvent, 385,425,255,295)){
+    		console.log("d");    		
+	    	$("#1_black_dot").off();
+	    }
+    } else if(dot_name === "2_black_dot"){
+    	if (inRange(currentEvent, 635,675,325,365)){
+     		console.log("e");   		
+	    	$("#2_black_dot").off();
+	    }
+    }
 	}
 
-	function snapIntoPlace(){
-		$("#blue_dot").animate({top: '110px', left: '181px'});
-		$("#blue_dot").off();
+	function inRange(currentEvent, xMin, xMax, yMin, yMax){
+			if (currentEvent.pageX > xMin && currentEvent.pageX < xMax
+    	&& currentEvent.pageY > yMin && currentEvent.pageY < yMax){
+				return true;
+    	} else {
+    		return false;
+    	}
 	}
 
 });
