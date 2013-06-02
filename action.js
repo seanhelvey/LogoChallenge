4af7be74e59d5955e0b04c108af546fea0e4d2bf
Sean Helvey <smh436@nyu.edu>
2013-06-02 14:16:17 -0600

    one DOM dot snaps into place

--- action.js
+++ action.js
@@ -1,7 +1,40 @@
 $(function() {	
-  $(".ui-widget-content").draggable();
+  $("[id$='dot']").draggable();
 
 	$("#reset_button").click(function() {
   	document.location.reload(true)
 	});
+
+  /*
+  $(".ui-widget-content").each(function( index ) {
+  	console.log( index + "left: " + $(this).position().left );
+  	console.log( index + "top: " + $(this).position().top );
+  	console.log( index + "right: " + $(this).position().right );
+   	console.log( index + "bottom: " + $(this).position().bottom ); 	  	  	
+	});*/
+
+	$("#blue_dot").mousemove(function(event) {
+  var msg = "Handler for .mousemove() called at ";
+  msg += event.pageX + ", " + event.pageY;
+	console.log(msg);
+	controlDotLocation(event)
+	});
+
+	function controlDotLocation(event){
+
+    if (event.pageX > 540 && event.pageX < 580
+    	&& event.pageY > 160 && event.pageY < 200) {
+    	console.log("got it!!!");
+    	snapIntoPlace();
+		//blue
+		//lr 530-590
+		//tp 150-210
+		}
+	}
+
+	function snapIntoPlace(){
+		$("#blue_dot").animate({top: '110px', left: '181px'});
+		$("#blue_dot").off();
+	}
+
 });
