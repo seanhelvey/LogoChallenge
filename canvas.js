3ee9e74d866a3bd5a781d9f78858c6476533ce46
Sean Helvey <smh436@nyu.edu>
2013-06-01 20:38:47 -0600

    multiple dots working on touch

--- canvas.js
+++ canvas.js
@@ -2,8 +2,9 @@ $(function() {
 
 var canvas;
 var ctx;
-var x = 40;
-var y = 40;
+var i = 0;
+var x = [40,140,240];
+var y = [40,40,40];
 var WIDTH = 400;
 var HEIGHT = 500;
 var dragok = false;
@@ -30,8 +31,6 @@ function init() {
  canvas = document.getElementById("canvas");
  ctx = canvas.getContext("2d");
 
-
-
  canvas.addEventListener("mousedown", mouseDown, false);
  canvas.addEventListener("mouseup", mouseUp, false);
  canvas.addEventListener("mousemove", mouseMove, false); 
@@ -39,10 +38,16 @@ function init() {
  canvas.addEventListener("touchend", touchUp, false);
  canvas.addEventListener("touchmove", touchMove, false);
 
- return setInterval(draw, 10);
+ return setInterval(hokeyPokey, 10);
 }
 
-function draw() {
+function hokeyPokey(){
+	draw1();
+	draw2();
+	draw3();
+}
+
+function draw1() {
  clear();
  ctx.fillStyle = "#FFFFFF";
  rect(0,0,WIDTH,HEIGHT);
@@ -53,23 +58,52 @@ function draw() {
  	img.src = 'images/ia-logo/ia-logo-back-copy.png'
   ctx.drawImage(img,0,100,400,400);
 }
+  arc(x[0], y[0]);
+}
 
-  arc(x, y);
+function draw2() {
+ //clear();
+ ctx.fillStyle = "#FFFFFF";
+ //rect(0,0,WIDTH,HEIGHT);
+ ctx.fillStyle = "#FF0000";
+
+/*
+ if (img == null){
+	var img = new Image();
+ 	img.src = 'images/ia-logo/ia-logo-back-copy.png'
+  ctx.drawImage(img,0,100,400,400);
+}*/
+
+ arc(x[1], y[1]);
+}
+
+function draw3() {
+ //clear();
+ ctx.fillStyle = "#FFFFFF";
+ //rect(0,0,WIDTH,HEIGHT);
+ ctx.fillStyle = "#FF0000";
+
+/*
+ if (img == null){
+	var img = new Image();
+ 	img.src = 'images/ia-logo/ia-logo-back-copy.png'
+  ctx.drawImage(img,0,100,400,400);
+}*/
+
+ arc(x[2], y[2]);
 }
 
 function mouseMove(e){
  if (dragok){
-  x = e.pageX - canvas.offsetLeft;
-  y = e.pageY - canvas.offsetTop;
+  x[i] = e.pageX - canvas.offsetLeft;
+  y[i] = e.pageY - canvas.offsetTop;
  }
 }
 
 function mouseDown(e){
- if (e.pageX < x + 15 + canvas.offsetLeft && e.pageX > x - 15 +
- canvas.offsetLeft && e.pageY < y + 15 + canvas.offsetTop &&
- e.pageY > y -15 + canvas.offsetTop){
-  x = e.pageX - canvas.offsetLeft;
-  y = e.pageY - canvas.offsetTop;
+ if ( test(e) ){
+  x[i] = e.pageX - canvas.offsetLeft;
+  y[i] = e.pageY - canvas.offsetTop;
   dragok = true;
   canvas.onmousemove = mouseMove;
  }
@@ -86,19 +120,43 @@ function touchMove(e){
   e.preventDefault();
 
  if (dragok){
-  x = e.pageX - canvas.offsetLeft;
-  y = e.pageY - canvas.offsetTop;
+  x[i] = e.pageX - canvas.offsetLeft;
+  y[i] = e.pageY - canvas.offsetTop;
  }
 }
 
 function touchDown(e){
- if (e.pageX < x + 15 + canvas.offsetLeft && e.pageX > x - 15 +
- canvas.offsetLeft && e.pageY < y + 15 + canvas.offsetTop &&
- e.pageY > y -15 + canvas.offsetTop){
-  x = e.pageX - canvas.offsetLeft;
-  y = e.pageY - canvas.offsetTop;
+ if ( test(e) ){
+  x[i] = e.pageX - canvas.offsetLeft;
+  y[i] = e.pageY - canvas.offsetTop;
   dragok = true;
  }
+}
+
+function test(e){
+var result = false;
+for(var iter = 0; iter < 3; iter++){
+ if (e.pageX < x[iter] + 15 + canvas.offsetLeft && 
+ 		 e.pageX > x[iter] - 15 + canvas.offsetLeft && 
+ 		 e.pageY < y[iter] + 15 + canvas.offsetTop &&
+ 		 e.pageY > y[iter] - 15 + canvas.offsetTop){
+ 	console.log(e.pageX);
+ 	console.log(e.pageY);
+     i = iter;
+   console.log(i);
+   console.log(x[iter]);
+   result = true;
+ }
+ else {
+ 	//console.log(e.pageX);
+ 	//console.log(e.pageY); 
+ 	console.log(x[iter]);	  
+ }
+
+}
+
+return result;
+
 }
 
 function mouseUp(){
