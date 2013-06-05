49a48c56b594ef4076fd5b576d3ca3be8b8dcff7
Sean Helvey <smh436@nyu.edu>
2013-06-04 18:04:02 -0600

    refactored DOM implementation. now using position relative to parent div.

--- action.js
+++ action.js
@@ -1,51 +1,19 @@
-$(function() {	
+$(function() {
 
-	var BBLUE = [540,580,160,200];
-	var IBLUE = [480,520,165,205];
-
-	var BRED = [660,700,180,220];
-	var IRED = [600,640,180,220];
-
-	var BGREEN = [450,490,355,395];
-	var IGREEN = [400,440,360,400];
-
-	var BBLACK1 = [385,425,255,295];
-	var IBLACK1 = [330,370,260,300];
-
-	var BBLACK2 = [635,675,325,365];
-	var IBLACK2 = [580,620,330,370];
-
-	var BLUE;
-	var RED;
-	var GREEN;
-	var BLACK1;
-	var BLACK2;
-
-	if( navigator.userAgent.match(/iPad/i) ) {
-		BLUE = IBLUE;
-		RED = IRED;
-		GREEN = IGREEN;
-		BLACK1 = IBLACK1;
-		BLACK2 = IBLACK2;								
-	} else {
-		BLUE = BBLUE;
-		RED = BRED;
-		GREEN = BGREEN;
-		BLACK1 = BBLACK1;
-		BLACK2 = BBLACK2;				
-	}
-
+  var BUFFER = 20;
+	var BLUE = [230,140];
+	var RED = [352,157];
+	var GREEN = [143,333];
+	var BLACK1 = [79,237];
+	var BLACK2 = [328,300];
 
   $("[id$='dot']").draggable();
   $("#logo").droppable({
 			drop: function( event, ui ) {
-				console.log(ui.helper.context.id);
-				var id = ui.helper.context.id;
-				console.log(event.originalEvent);
-				//var touch = event.originalEvent.touches[0].pageX;
-				//$("#debugx").text(event.originalEvent.type);
-				$("#debugx").text(event.originalEvent.pageX);
-				$("#debugy").text(event.originalEvent.pageY);  
+				console.log(event.toElement.id);
+				console.log($("#"+ event.toElement.id).position().left);
+				console.log($("#"+ event.toElement.id).position().top);
+				locationControl(event.toElement.id,event);
   		}
   })
 
@@ -53,56 +21,43 @@ $(function() {
   	document.location.reload(true)
 	});
 
-	detectMouse("blue_dot");
-	detectMouse("red_dot");
-	detectMouse("green_dot");
-	detectMouse("1_black_dot");
-	detectMouse("2_black_dot");
-
-  function detectMouse(dot_name){
-		$("#" + dot_name).mouseup(function(event) {
-			  var msg = "Handler for .mouseup() called at ";
-			  msg += event.pageX + ", " + event.pageY;
-				console.log(msg);
-				locationControl(dot_name,event);
-			});
-  }
-
 	function locationControl(dot_name, currentEvent){
     if (dot_name === "blue_dot"){
     	console.log(BLUE[0]);
-    	if (inRange(currentEvent,BLUE[0],BLUE[1],BLUE[2],BLUE[3])){
+    	if (inRange(currentEvent,BLUE)){
     		$("#debugz").text("hi");     		
     		console.log("a");
     		$("#blue_dot").off();
     	}
     } else if(dot_name === "red_dot"){
 	    	console.log("xxx"); 
-    	if (inRange(currentEvent,RED[0],RED[1],RED[2],RED[3])){
+    	if (inRange(currentEvent,RED)){
     		console.log("b");    		
     		$("#red_dot").off();
     	}
     } else if(dot_name === "green_dot"){
-    	if (inRange(currentEvent,GREEN[0],GREEN[1],GREEN[2],GREEN[3])){
+    	if (inRange(currentEvent,GREEN)){
     		console.log("c");    		
 	    	$("#green_dot").off();
 	    }
     } else if(dot_name === "1_black_dot"){
-    	if (inRange(currentEvent,BLACK1[0],BLACK1[1],BLACK1[2],BLACK1[3])){
+    	if (inRange(currentEvent,BLACK1)){
     		console.log("d");    		
 	    	$("#1_black_dot").off();
 	    }
     } else if(dot_name === "2_black_dot"){
-    	if (inRange(currentEvent,BLACK2[0],BLACK2[1],BLACK2[2],BLACK2[3])){
+    	if (inRange(currentEvent,BLACK2)){
      		console.log("e");   		
 	    	$("#2_black_dot").off();
 	    }
     }
 	}
 
-	function inRange(currentEvent, xMin, xMax, yMin, yMax){
-			if (currentEvent.pageX > xMin && currentEvent.pageX < xMax
-    	&& currentEvent.pageY > yMin && currentEvent.pageY < yMax){
+	function inRange(currentEvent, colorMinMax){
+			var x = $("#"+ event.toElement.id).position().left;
+			var y = $("#"+ event.toElement.id).position().top;
+			if (x > colorMinMax[0] - BUFFER && x < colorMinMax[0] + BUFFER
+    	&& y > colorMinMax[1] - BUFFER && y < colorMinMax[1] + BUFFER){
 				return true;
     	} else {
     		return false;
