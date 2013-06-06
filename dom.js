9e5959c0ca3bd4efb109276b5dc5a17246490f12
Sean Helvey <smh436@nyu.edu>
2013-06-06 12:56:42 -0600

    objectified DOM implementation

--- dom.js
+++ dom.js
@@ -1,48 +1,52 @@
-$(function() {
+function init(){
 
   var BUFFER = 20;
-	var BLUE = [230,140];
-	var RED = [352,157];
-	var GREEN = [143,333];
-	var BLACK1 = [79,237];
-	var BLACK2 = [328,300];
+  var BLUE = [230,140];
+  var RED = [352,157];
+  var GREEN = [143,333];
+  var BLACK1 = [79,237];
+  var BLACK2 = [328,300];
 
-  $("#reset_button").click(function() {
-    document.location.reload(true)
-  });
+  var DomLogo = function(){
+    $("[id$='dot']").draggable();
 
-  $("[id$='dot']").draggable();
-  $("#logo").droppable({
-		drop: function( event, ui ) {
-			locationControl(event.toElement.id,event);       
-		}
-  })
+    var dom_logo = this;
+    $("#logo").droppable({
+      drop: function(e, ui) {
+        dom_logo.locationControl(e.toElement.id,event);       
+      }
+    })
 
-	function locationControl(dot_name, currentEvent){    
+    $("#reset_button").click(function() {
+      document.location.reload(true)
+    });
+  };
+
+	DomLogo.prototype.locationControl = function(dot_name, e){   
     if (dot_name === "blue_dot"){
-    	if (inRange(currentEvent,BLUE)){ 
+    	if (this.inRange(e,BLUE)){ 
     		$("#blue_dot").off();
     	}
     } else if(dot_name === "red_dot"){
-    	if (inRange(currentEvent,RED)){
+    	if (this.inRange(e,RED)){
     		$("#red_dot").off();
     	}
     } else if(dot_name === "green_dot"){
-    	if (inRange(currentEvent,GREEN)){
+    	if (this.inRange(e,GREEN)){
 	    	$("#green_dot").off();
 	    }
     } else if(dot_name === "1_black_dot"){
-    	if (inRange(currentEvent,BLACK1)){
+    	if (this.inRange(e,BLACK1)){
 	    	$("#1_black_dot").off();
 	    }
     } else if(dot_name === "2_black_dot"){
-    	if (inRange(currentEvent,BLACK2)){ 		
+    	if (this.inRange(e,BLACK2)){ 		
 	    	$("#2_black_dot").off();
 	    }
     }
 	}
 
-	function inRange(currentEvent, colorMinMax){    
+  DomLogo.prototype.inRange = function(currentEvent, colorMinMax){
 		var x = $("#"+ event.toElement.id).position().left;
 		var y = $("#"+ event.toElement.id).position().top;  
 		if (x > colorMinMax[0] - BUFFER && x < colorMinMax[0] + BUFFER
@@ -53,4 +57,8 @@ $(function() {
   	}
 	}
 
-});
+  var domLogo = new DomLogo();
+
+}
+
+$(init);
